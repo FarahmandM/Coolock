fc0a0c91fff71e137dc11c9dbd69e775698a21a9
FarahmandM <CivilForYou@yahoo.com>
2017-03-02 01:33:28 +0330

    Essential changes

--- coolock.js
+++ coolock.js
@@ -1,6 +1,6 @@
 /*
 Coolock is a cool clock that is using pure HTML5 and JavaScript.
-Copyright (C) 2013  Farahmand Moslemi (CivilForYou) URL: https://github.com/CivilForYou/Coolock
+Copyright (C) Farahmand Moslemi (FarahmandM) URL: https://github.com/FarahmandM/Coolock
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -17,86 +17,79 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-function Coolock(CWidth, CHeight)
-{
-	var c = document.createElement("canvas");
-	c.id = 'coolock';
-	c.width = CWidth;
-	c.height = CHeight;
+/* 
+ * Coolock Class Definition
+ */
+function Coolock(options) {
+	var defaultOptions = {size: 400, canvasId: 'coolock', canvasWrapperId: 'coolockWrapper', strokeColor: '#000', fillColor: '#fff', lineCap: 'round'}
+	options = Object.assign({}, defaultOptions, options);
 	
-	document.getElementById('coolockWrapper').appendChild(c);
-	var ctx = c.getContext("2d");
-
-	ctx.strokeStyle = '#000';
-	ctx.fillStyle = '#fff';
-	ctx.lineWidth = 0.04 * Math.min(CWidth, CHeight);
-	ctx.beginPath();
-	ctx.arc(CWidth / 2,CHeight / 2, 0.42 * Math.min(CWidth, CHeight), 0, 2 * Math.PI);
-	ctx.closePath();
-	ctx.stroke();
-
-	ctx.lineWidth = 0.006 * Math.min(CWidth, CHeight);
-	for(i = 0; i < 60; i++)
-	{
+	[this.size, this.canvasId, this.canvasWrapperId, this.strokeColor, this.fillColor, this.lineCap] = [options.size, options.canvasId, options.canvasWrapperId, options.strokeColor, options.fillColor, options.lineCap];
+	
+	var canvas = document.createElement("canvas");
+	[canvas.id, canvas.width, canvas.height] = [this.canvasId, this.size, this.size];
+	
+	document.getElementById(this.canvasWrapperId).appendChild(canvas);
+	var ctx = canvas.getContext("2d");
+	this.ctx = ctx;
+	
+	this.drawCircle = function(x, y, r, w, stroke = true, fill = false) {
+		ctx = this.ctx;
+		
+		[ctx.strokeStyle, ctx.fillStyle, ctx.lineWidth] = [this.strokeColor, this.fillColor, w];
 		ctx.beginPath();
-		ctx.arc(CWidth / 2 + 0.36 * Math.min(CWidth, CHeight) * Math.cos(6 * i * Math.PI / 180),CHeight / 2 + 0.36 * Math.min(CWidth, CHeight) * Math.sin(6 * i * Math.PI / 180), 0.006 * Math.min(CWidth, CHeight), 0, 2 * Math.PI);
-		ctx.closePath();
-		ctx.stroke();
+		ctx.arc(x, y, r, 0, 2 * Math.PI);
+		stroke && ctx.stroke();
+		fill && ctx.fill();
 	}
 	
-	ctx.lineWidth = 0.01 * Math.min(CWidth, CHeight);
-	for(i = 0; i < 60; i+= 5)
-	{
+	this.drawLine = function(x1, y1, x2, y2, w, stroke = true, fill = false, closePath = false) {
+		ctx = this.ctx;
+		
+		[ctx.strokeStyle, ctx.fillStyle, ctx.lineCap, ctx.lineWidth] = [this.strokeColor, this.fillColor, this.lineCap, w];
 		ctx.beginPath();
-		ctx.arc(CWidth / 2 + 0.36 * Math.min(CWidth, CHeight) * Math.cos(6 * i * Math.PI / 180),CHeight / 2 + 0.36 * Math.min(CWidth, CHeight) * Math.sin(6 * i * Math.PI / 180), 0.01 * Math.min(CWidth, CHeight), 0, 2 * Math.PI);
-		ctx.closePath();
-		ctx.stroke();
+		ctx.moveTo(x1, y1);
+		ctx.lineTo(x2, y2);
+		stroke && ctx.stroke();
+		fill && ctx.fill();
+		closePath && ctx.closePath();
 	}
 	
-	function _coolock()
-	{
+	this.draw = function() {
+		ctx = this.ctx;
+		
+		this.drawCircle(this.size / 2, this.size / 2, 0.42 * this.size, 0.08 * this.size, true, true);
+
+		for(i = 0; i < 60; i++) {
+			this.drawCircle(this.size / 2 + 0.36 * this.size * Math.cos(6 * i * Math.PI / 180), this.size / 2 + 0.36 * this.size * Math.sin(6 * i * Math.PI / 180), 0.006 * this.size, 0.006 * this.size, true);
+		}
+		
+		for(i = 0; i < 60; i+= 5) {
+			this.drawCircle(this.size / 2 + 0.36 * this.size * Math.cos(6 * i * Math.PI / 180), this.size / 2 + 0.36 * this.size * Math.sin(6 * i * Math.PI / 180), 0.01 * this.size, 0.01 * this.size, true);
+		}
+	}
+	
+	this.redraw = function() {
+		var ctx = this.ctx;
+
 		d = new Date();
 		h = d.getHours();
 		m = d.getMinutes();
 		s = d.getSeconds();
 	
-		ctx.fillStyle = '#fff';
+		this.drawCircle(this.size / 2, this.size / 2, 0.333 * this.size, 0.01 * this.size, false, true);
+		this.drawLine(this.size / 2 + 0.19 * this.size * Math.cos(30 * (h + m / 60 + s / 3600) * Math.PI / 180 - Math.PI / 2), this.size / 2 + 0.19 * this.size * Math.sin(30 * (h + m / 60 + s / 3600) * Math.PI / 180 - Math.PI / 2), this.size / 2, this.size / 2, 0.04 * this.size)
+		this.drawLine(this.size / 2 + 0.31 * this.size * Math.cos(6 * (m + s / 60) * Math.PI / 180 - Math.PI / 2), this.size / 2 + 0.31 * this.size * Math.sin(6 * (m + s / 60) * Math.PI / 180 - Math.PI / 2), this.size / 2, this.size / 2, 0.03 * this.size)
+		this.drawLine(this.size / 2 + 0.32 * this.size * Math.cos(6 * s * Math.PI / 180 - Math.PI / 2), this.size / 2 + 0.32 * this.size * Math.sin(6 * s * Math.PI / 180 - Math.PI / 2), this.size / 2, this.size / 2, 0.01 * this.size)
 		
+		ctx.fillStyle = this.strokeColor;
 		ctx.beginPath();
-		ctx.arc(CWidth / 2,CHeight / 2, 0.3301 * Math.min(CWidth, CHeight), 0, Math.PI * 2, true);
-		ctx.closePath();
-		ctx.fill();
-		
-		ctx.lineCap = 'round';
-		ctx.lineWidth = 0.04 * Math.min(CWidth, CHeight);
-		ctx.beginPath();
-		ctx.moveTo(CWidth / 2 + 0.19 * Math.min(CWidth, CHeight) * Math.cos(30 * (h + m / 60 + s / 3600) * Math.PI / 180 - Math.PI / 2), CHeight / 2 + 0.19 * Math.min(CWidth, CHeight) * Math.sin(30 * (h + m / 60 + s / 3600) * Math.PI / 180 - Math.PI / 2));
-		ctx.lineTo(CWidth / 2, CHeight / 2);
-		ctx.closePath();
-		ctx.stroke();
-		
-		ctx.lineWidth = 0.03 * Math.min(CWidth, CHeight);
-		ctx.beginPath();
-		ctx.moveTo(CWidth / 2 + 0.31 * Math.min(CWidth, CHeight) * Math.cos(6 * (m + s / 60) * Math.PI / 180 - Math.PI / 2), CHeight / 2 + 0.31 * Math.min(CWidth, CHeight) * Math.sin(6 * (m + s / 60) * Math.PI / 180 - Math.PI / 2));
-		ctx.lineTo(CWidth / 2, CHeight / 2);
-		ctx.closePath();
-		ctx.stroke();
-	
-		ctx.lineWidth = 0.01 * Math.min(CWidth, CHeight);
-		ctx.beginPath();
-		ctx.moveTo(CWidth / 2 + 0.33 * Math.min(CWidth, CHeight) * Math.cos(6 * s * Math.PI / 180 - Math.PI / 2), CHeight / 2 + 0.33 * Math.min(CWidth, CHeight) * Math.sin(6 * s * Math.PI / 180 - Math.PI / 2));
-		ctx.lineTo(CWidth / 2, CHeight / 2);
-		ctx.closePath();
-		ctx.stroke();
-		
-		ctx.fillStyle = '#000';
-		ctx.beginPath();
-		ctx.arc(CWidth / 2, CHeight / 2, 0.04 * Math.min(CWidth, CHeight), 0, Math.PI * 2, true);
-		ctx.closePath();
+		ctx.arc(this.size / 2, this.size / 2, 0.04 * this.size, 0, Math.PI * 2);
 		ctx.fill();
 	
-		setTimeout(_coolock, 1000);
+		setTimeout(this.redraw.bind(this), 1000);
 	}
-
-	_coolock();
+	
+	this.draw();
+	this.redraw();
 }
